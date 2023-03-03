261ce5b0d474b8479f83c00f18ae705dc9044a1d
saibia8 <saibia.design@gmail.com>
2023-03-03 13:36:01 +0100

    Many things

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navigation
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -10,3 +11,17 @@ document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click", ()
    hamburger.classList.remove("active");
    navMenu.classList.remove("active");
 }));
+
+// Cursor
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e)=> {
+   cursor.style.left = e.pageX + "px";
+   cursor.style.top = e.pageY + "px";
+});
+
+// To the top of the page
+const toTop = document.querySelector("#toTop");
+toTop.addEventListener("click", ()=> {
+  
+   window.scrollTo({ top: 0, behavior: "smooth"});
+});
